5fe6b1745a82c788f7aaba4cf9c464ea3f1d2b3c
yukinoshiita <850465769@qq.com>
2020-07-11 12:05:57 -0700

    debug for combo_check(excluding two-card combo)

--- card.cpp
+++ card.cpp
@@ -8,7 +8,7 @@ Card::Card(){
 		this -> instruction 	= "";
 }
 
-Card::Card(unsigned int num){
+Card::Card(int num){
 		this -> number=num;
 		this -> name=NAME[num];
 		this -> instruction=INS[num];
@@ -36,40 +36,44 @@ Damage Card::deal_damage(){
 	Damage result;
 	result.damage=1;
 	//special cards for the light and dark
-	if (this->number==LIGHT||this->number==DARK) result.damage=2;
-	result.star=ATTACK[this->number];
-	result.type=this->number;
+	if (number==LIGHT||number==DARK) result.damage=2;
+	result.star=ATTACK[number];
+	result.type=number;
 	return result;
 }
 
-static enum Combo combo_check(int card_num_in_combo;vector <Card> card_in_combo){
+int Card::identify(){return this->number;}
+
+static enum Combo combo_check(int card_amount_in_combo, std::vector<int> card_number_in_combo){
 	enum Combo result;
-	switch (card_num_in_combo){
+	switch (card_amount_in_combo){
 		case 2: break;//unfinished
 		case 3: //finished
 		case 4:	//use recursion to check the element combo
-			switch (card_in_combo[card_num_in_combo-1].number){
+			switch (card_number_in_combo[card_amount_in_combo-1]){
 				case WINDY:
 				case FIREY:
 				case WATERY:
-				case EARTHY: result=combo_check(card_num_in_combo-1,card_in_combo);
-					     if (result!=invalid) 
-						     if(card_num_in_combo==3)
+				case EARTHY: result=combo_check(card_amount_in_combo-1,card_number_in_combo);
+					     if (result!=Invalid) 
+						     if(card_amount_in_combo==3)
 							     result=Triple_Element;
 						     else result=Quadra_Element;
 					     break;
-				default: result=invalid;break;
+				default: result=Invalid;break;
 			}break;
-		default: result=invalid;break;
+		default: result=Invalid;break;
 	}
 	return result;
 }
 
 Windy::Windy():Card(0){}
 
-enum Combo Windy::combo(int card_num_in_combo;vector <Card> card_in_combo){
+enum Combo Windy::combo(int card_amount_in_combo,std::vector <Card> card_in_combo){
 	//add this card to the vector
-	Card this_card(this->number);
+	Card this_card(number);
 	card_in_combo.push_back(this_card);
-	return combo_check(card_num_in_combo,card_in_combo);
+	std::vector <int> card_number_in_combo;
+	for (int i=0;i<card_amount_in_combo;i++) card_number_in_combo.push_back(card_in_combo[i].identify());
+	return combo_check(card_amount_in_combo,card_number_in_combo);
 }
--- card.h
+++ card.h
@@ -1,34 +1,37 @@
 #ifndef _CARD_H_
 #define _CARD_H_
 #include <string>
+#include <vector>
 #include "card_info.h"
 
 enum Combo {Invalid,Double_Element,Element_Arrow,Element_Sword,Freezing_Rain,Invisible,Light_Dark,Maze_Illusion,Maze_Lord,Nightmare,Powerful_Light,Quadra_Element,The_Big,The_Twin,Thunder_Shot,Time_Return,Triple_Element,Water_Shield};
 
-struct DAMAGE{
+typedef struct DAMAGE{
 	int damage;
 	int star;
 	int type;//used for some special attacking-defending rules
 }Damage;
 
 class Card{
-private:
+protected:
 	//information to display to the player
-	unsigned int number;
+	int number;
 	std::string name;
 	std::string instruction;
 public:
 	Card ();
+	Card (int num);
 	void display();//show the information of the single card
 	void operator = (Card card_to_assign);
 	bool operator == (Card card_to_compare); 
 	Damage deal_damage();
+	int identify();
 };
 
-class Windy:public Card(){
+class Windy:public Card{
 public:
 	Windy ();
-	enum Combo combo(int card_num_in_combo, Card* card_in_combo);//return the type of the combo
+	enum Combo combo(int card_amount_in_combo, std::vector <Card> card_in_combo);//return the type of the combo
 };
 
 #endif
